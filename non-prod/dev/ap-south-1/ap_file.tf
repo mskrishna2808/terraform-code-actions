78c1c11bb9cf84d1535c718f292046cf504fd180
Mendu Sai krishna <mskrishna2808@gmail.com>
2024-03-13 11:34:53 +0530

    checking terraform actions

---
+++ non-prod/dev/ap-south-1/ap_file.tf
@@ -0,0 +1,5 @@
+resource "local_file" "ap-south-1" {
+    filename = "ap-south-1.txt"
+    content = "This is Mumbai region"
+  
+}
---
+++ prod/staging/us-east-1/us_file.tf
@@ -0,0 +1,5 @@
+resource "local_file" "ap-south-1" {
+    filename = "us-east-1.txt"
+    content = "This is US region"
+  
+}
